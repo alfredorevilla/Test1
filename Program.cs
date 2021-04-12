90b1a5a08de4bb6d37e70e3fe1b5b8ed1cb6668f
Alfredo Revilla <alfredorevilla@gmail.com>
2021-04-12 13:42:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Test1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static bool Contains(int[] array, int number)
+        {
+            int length;
+            if (array == null || (length = array.Length) == 0)
+            {
+                return false;
+            }
+
+            if (array[0] > number || array[^1] < number)
+            {
+                return false;
+            }
+
+            var left = 0;
+            var right = length - 1;
+
+            do
+            {
+                var index = (int)Math.Floor((decimal)(left + right) / 2);
+                var current = array[index];
+                if (current < number)
+                {
+                    left = index + 1;
+                }
+                else if (current > number)
+                {
+                    right = index - 1;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            while (left <= right);
+            return false;
+        }
+
+        public static bool Contains(int[][] array, int number)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return false;
+            }
+
+            foreach (var array2 in array)
+            {
+                if (Contains(array2, number))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+ # Find if a number is contained within a sorted 2 dimensional array
+
+### Pre-requisites
+Dot.Net Core 3.1
+
+### Build
+From command line:
+```
+dotnet build
+```
+
+### Test
+From command line:
+```
+dotnet test
+```
